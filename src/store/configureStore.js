33cca3d246adddd2af7fc6bea39c0ba8b11057a1
Maxim Chervyakov <maxim.chervyakov@purrweb.com>
2020-09-14 13:49:11 +0600

    persisted storage

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,7 +1,16 @@
 import { createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import { boardApp } from './reducers';
 
-const store = createStore(boardApp, composeWithDevTools());
-export default store;
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, boardApp);
+
+export const store = createStore(persistedReducer, composeWithDevTools());
+export const persistor = persistStore(store);
